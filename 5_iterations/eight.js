974431d013d2b94e4614f8b43532d9268a9dcb7b
rushikesh109 <mangrulerushikesh2003@gmail.com>
2024-06-02 21:17:42 +0530

    reduce & shoppingCart

---
+++ 5_iterations/eight.js
@@ -0,0 +1,31 @@
+const myNums = [1,2,3]
+
+// const myTotal = myNums.reduce(function(acc, currval){
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// },0)
+
+// console.log(myTotal);
+
+// const myTotal = myNums.reduce((acc, curr)=>acc+curr,0)
+// console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price:2999
+    },
+    {
+        itemName: "cpp course",
+        price:999
+    },
+    {
+        itemName: "python course",
+        price:99
+    }
+]
+
+const priceToPay=shoppingCart.reduce((acc,item)=>
+     acc + item.price,0)
+
+console.log(priceToPay);
